acfc183efdb61fdad7450d0f3dd2235895ff9016
Robby Ryder <Rryder1@Roberts-MacBook-Pro.local>
2017-05-31 21:42:05 -0400

    Added Array Function

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = EmojiArray[indexPath.row]
+        return cell
+        
+    }
+    
+    //Array of Emojis
+    
+    var EmojiArray = ["😀","🎾","🤑","😻","💩","🥇","🍆"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (EmojiArray.count)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
